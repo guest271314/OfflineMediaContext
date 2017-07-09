422502539b5d6df02ed5a3d9c7940b9a8aea29a4
guest271314 <guest271314@gmail.com>
2017-07-09 13:41:20 -0700

    Update OfflineMediaContext-Gecko.js

--- OfflineMediaContext-Gecko.js
+++ OfflineMediaContext-Gecko.js
@@ -3,7 +3,7 @@
 // Fetch media resource and create independent media fragments of a given range of bytes 
 // or time slices capable of being played individually as fast as possible.
 
-// Modelled on OfflineAudioContext.
+// Modeled on OfflineAudioContext.
 
     class OfflineMediaContext {
       constructor({
